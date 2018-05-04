145525c9f1d46d2f53f45ea0d74ce8b467ca32fd
barnettm <barnettm@student.ethz.ch>
2018-05-04 20:49:25 +0530

    cube beautifully aligned

--- vtk_example.py
+++ vtk_example.py
@@ -22,6 +22,8 @@ from detection_3D import capture
 import numpy as np
 from numpy.linalg import inv
 from order_pts import order_pts
+
+
 def draw(img, corner, imgpts):
     corner = tuple(corner)
     img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
@@ -29,24 +31,24 @@ def draw(img, corner, imgpts):
     img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
     return img
 
+
 def draw_cage(img, corner, imgpts):
-    imgpts = np.int32(imgpts).reshape(-1,2)
+    imgpts = np.int32(imgpts).reshape(-1, 2)
 
     # draw ground floor in green
-    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+    img = cv2.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
 
     # draw pillars in blue color
-    for i,j in zip(range(4),range(4,8)):
-        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+    for i, j in zip(range(4), range(4, 8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]), (255), 3)
 
     # draw top layer in red color
-    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255), 3)
+    img = cv2.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
 
     return img
 
 
-def get_vectors(image, points, mtx, dist):
-
+def get_vectors(image, points, coord, mtx, dist):
     # order points
     points = order_pts(points)
 
@@ -57,46 +59,40 @@ def get_vectors(image, points, mtx, dist):
 
     imgp = np.array(points, dtype="float32")
 
-    objp = np.array([[0.,0.,0.],[1.,0.,0.],
-                        [1.,1.,0.],[0.,1.,0.]], dtype="float32")
+    objp = np.array(coord, dtype="float32")
 
     # calculate rotation and translation vectors
-    imgp = cv2.cornerSubPix(gray,imgp,(11,11),(-1,-1),criteria)
+    imgp = cv2.cornerSubPix(gray, imgp, (11, 11), (-1, -1), criteria)
     _, rvecs, tvecs = cv2.solvePnP(objp, imgp, mtx, dist)
 
     return rvecs, tvecs
 
 
 def main(argv):
-
+    # set window size
     width = 640
     height = 480
 
-    # axis to be displayed on glyph
-    axis = np.float32([[0,0,0], [0,1,0], [1,1,0], [1,0,0],
-                   [0,0,-1],[0,1,-1],[1,1,-1],[1,0,-1] ])    # load calibration data
+    # load camera calibration data
     with np.load('camcalib.npz') as X:
         mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
-
+    # set focal length
     focalLengthY = mtx[1][1]
-    # factor = windowSize.height / ImageSize.height
-    # focalLengthY = focalLengthY * factor
-    viewAngle = 2* np.arctan(height /2 / focalLengthY) * 180 / np.pi
-
-    px = mtx[0][2]
-    py = mtx[1][2]
-    cx = width - px
-    cy = py
-
-    windowCenterX = cx / (width-1)/2 - 1
-    windowCenterY = cy / (height-1)/2 - 1
+    viewAngle = 2 * np.arctan(height / 2 / focalLengthY) * 180 / np.pi
 
 
+    # Object 3: cage_axis to be displayed on glyph
+    cage_axis = np.float32([[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0],
+                            [0, 0, -1], [0, 1, -1], [1, 1, -1], [1, 0, -1]])
+
+
+
+    # read first frame
     cam = cv2.VideoCapture(0)
     ret, frame = cam.read()
     cv2.imwrite('webcam.jpg', frame)
 
-    #  Verify input arguments
+    # Create background with webcam input
     # Read the image
     path = 'webcam.jpg'
     jpeg_reader = vtkJPEGReader()
@@ -108,10 +104,8 @@ def main(argv):
     jpeg_reader.Update()
     image_data = jpeg_reader.GetOutput()
 
-
     # Create an image actor to display the image
     image_actor = vtkImageActor()
-
     if VTK_MAJOR_VERSION <= 5:
         image_actor.SetInput(image_data)
     else:
@@ -120,9 +114,10 @@ def main(argv):
     # Create a renderer to display the image in the background
     background_renderer = vtkRenderer()
 
+
+    # Object 1: Cube
     # Read .obj
-    #file_name = 'data/3d_tree/3d_tree.obj'
-    file_name = 'data/totoro_target.obj'
+    file_name = 'data/3d_tree/3d_tree.obj'
     reader = vtk.vtkOBJReader()
     reader.SetFileName(file_name)
     reader.Update()
@@ -133,14 +128,35 @@ def main(argv):
 
     CubeActor = vtk.vtkActor()
     CubeActor.SetMapper(modelMapper)
+    cube_coord = [[-1., -1., 0.], [1., -1., 0.],
+                  [1., 1., 0.], [-1., 1., 0.]]
+
+    # Object 2: Totoro
+    # Read .obj
+    file_name = 'data/totoro_target.obj'
+    reader = vtk.vtkOBJReader()
+    reader.SetFileName(file_name)
+    reader.Update()
+    poly_data = reader.GetOutput()
+
+    totoroMapper = vtk.vtkPolyDataMapper()
+    totoroMapper.SetInputData(poly_data)
+
+    totoroActor = vtk.vtkActor()
+    totoroActor.SetMapper(totoroMapper)
+    totoro_coord = [[-1., -1., 0.], [1., -1., 0.],
+                  [1., 1., 0.], [-1., 1., 0.]]
 
 
+
+    # create renderer to display objects
     scene_renderer = vtkRenderer()
 
+    # create render window to combine all renderer
     render_window = vtkRenderWindow()
     render_window.SetSize(width, height)
 
-    # Set up the render window and renderers such that there is
+    # Set up the render window and renderer such that there is
     # a background layer and a foreground layer
     background_renderer.SetLayer(0)
     background_renderer.InteractiveOff()
@@ -152,15 +168,12 @@ def main(argv):
     render_window_interactor = vtkRenderWindowInteractor()
     render_window_interactor.SetRenderWindow(render_window)
 
-    # Add actors to the renderers
-    # scene_renderer.AddActor(superquadric_actor)
+    # Add actors to the renderer
     background_renderer.AddActor(image_actor)
     scene_renderer.AddActor(CubeActor)
 
     # Render once to figure out where the background camera will be
     render_window.Render()
-
-
     # Set up the background camera to fill the renderer with the image
     origin = image_data.GetOrigin()
     spacing = image_data.GetSpacing()
@@ -168,9 +181,8 @@ def main(argv):
 
     camera = background_renderer.GetActiveCamera()
     camera.ParallelProjectionOn()
-
-    xc = origin[0] + 0.5*(extent[0] + extent[1]) * spacing[0]
-    yc = origin[1] + 0.5*(extent[2] + extent[3]) * spacing[1]
+    xc = origin[0] + 0.5 * (extent[0] + extent[1]) * spacing[0]
+    yc = origin[1] + 0.5 * (extent[2] + extent[3]) * spacing[1]
     # xd = (extent[1] - extent[0] + 1) * spacing[0]
     yd = (extent[3] - extent[2] + 1) * spacing[1]
     d = camera.GetDistance()
@@ -178,56 +190,28 @@ def main(argv):
     # camera.SetFocalPoint(xc, yc, 0.0)
     # camera.SetPosition(xc, yc, d)
 
+    # setup object camera according to webcam intrinsics
     obj_camera = scene_renderer.GetActiveCamera()
-    #obj_camera.SetViewAngle(viewAngle)
-    #obj_camera.SetWindowCenter(windowCenterX, windowCenterY)
-
-
+    obj_camera.SetViewAngle(viewAngle)
 
     # Render again to set the correct view
     render_window.Render()
 
-    count = 0
-
+    # start loop
+    count0 = 0
+    count1 = 0
     while True:
         ret, frame = cam.read()
 
         idx, approx = capture(frame)
-        if idx == 0 or idx == 1:
+        
+        if idx == 0:
+            scene_renderer.AddActor(CubeActor)
             (tl, tr, br, bl) = order_pts(approx)
-            rvecs, tvecs = get_vectors(frame, approx, mtx, dist)
-            imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
-
+            rvecs, tvecs = get_vectors(frame, approx, cube_coord, mtx, dist)
             rmat, _ = cv2.Rodrigues(rvecs)
 
-            # method 1
-            #rmatH = [[0, 0, 0, 0] for x in range(4)]
-            # rmatH = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]], np.float64)
-            # rmatH[0][0:3] = rmat[0][0:3]
-            # rmatH[1][0:3] = rmat[1][0:3]
-            # rmatH[2][0:3] = rmat[2][0:3]
-            # rmatH[0][3] = tvecs[0]
-            # rmatH[1][3] = tvecs[1]
-            # rmatH[2][3] = tvecs[2]
-
-            # poke = vtk.vtkMatrix4x4()
-            #
-            # for i in range(3):
-            #     for n in range(3):
-            #         poke.SetElement(i, n, rmat[i, n])
-            #     poke.SetElement(i, 3, tvecs[i])
-            #
-            # vtk_camera = obj_camera.GetViewTransformMatrix()
-            # rmatInv = inv(rmatH)
-            # poke.Invert()
-            # poke.Multiply4x4(poke, vtk_camera, vtk_camera)
-            # camera_transform = vtk.vtkTransform()
-            # camera_transform.SetMatrix(vtk_camera)
-            # obj_camera.ApplyTransform(camera_transform)
-            # obj_camera.SetWindowCenter(windowCenterX, windowCenterY)
-
             # method 2
-            # scale matrix??
             rmat[1][0] *= -1
             rmat[1][1] *= -1
             rmat[1][2] *= -1
@@ -237,7 +221,6 @@ def main(argv):
             tvec = tvecs.copy()
             tvec[1] *= -1
             tvec[2] *= -1
-            # normalize rows??
 
             viewPlaneNormal = (rmat[2][0].copy(), rmat[2][1].copy(), rmat[2][2].copy())
             rmatINV = rmat.copy()
@@ -246,39 +229,34 @@ def main(argv):
             translation = rmatINV.dot(tvec)
             translation *= -1
 
-
             # defines depth position of cube
             obj_camera.SetPosition(translation[0], translation[1], translation[2])
 
-            # this line makes the cube vanish
-            obj_camera.SetFocalPoint(translation[0][0]-viewPlaneNormal[0], translation[1][0]-viewPlaneNormal[1], translation[2][0]-viewPlaneNormal[2])
-            #obj_camera.SetFocalPoint(tvec[0], tvec[1], tvec[2])
-            #obj_camera.SetWindowCenter(windowCenterX, windowCenterY)
+            obj_camera.SetFocalPoint(translation[0][0] - viewPlaneNormal[0], translation[1][0] - viewPlaneNormal[1],
+                                     translation[2][0] - viewPlaneNormal[2])
 
             obj_camera.SetViewUp(rmat[1][0], rmat[1][1], rmat[1][2])
 
-
             scene_renderer.ResetCameraClippingRange()
 
-            count = 10;
+            count0 = 10;
+
+        if idx == 1:
+            (tl, tr, br, bl) = order_pts(approx)
+            rvecs, tvecs = get_vectors(frame, approx, cube_coord, mtx, dist)
+            imgpts, jac = cv2.projectPoints(cage_axis, rvecs, tvecs, mtx, dist)
+            count1 = 10;
 
         # Action Nr. 2: Draw cube on image
-        if count > 0:
-            frame = draw_cage(frame,tl, imgpts)
-            count -= 1;
+        if count1 > 0:
+            frame = draw_cage(frame, tl, imgpts)
+            count1 -= 1
+
+        count0 -= 1
+        if count0 <= 0:
+            scene_renderer.RemoveActor(CubeActor)
 
         cv2.imwrite('webcam.jpg', frame)
-
-        # Action Nr. 1: translate cube according to glyph
-        # if idx == 0:
-        #     print(idx, approx)
-        #     # scene_renderer.AddActor(CubeActor)
-        #     CubeActor.AddPosition(0.1, 0, 0)
-        # elif idx == 1:
-        #     print(idx, approx)
-        #     # scene_renderer.AddActor(CubeActor)
-        #     CubeActor.AddPosition(-0.1, 0, 0)
-
 
         # Read the image to background
         path = 'webcam.jpg'
@@ -296,7 +274,6 @@ def main(argv):
             image_actor.SetInputData(image_data)
 
         render_window.Render()
-
 
     # Interact with the window / keeps window open otherwise closes after loop is finished
     render_window_interactor.Start()
